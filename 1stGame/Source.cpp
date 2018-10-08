afa5ec695c6dc64f6436e214174076fdd3b8f1c9
Laviniu-Ioan Coita <laviniu.coita@student.upt.ro>
2018-10-08 17:42:39 +0300

    Create window

--- 1stGame/Source.cpp
+++ 1stGame/Source.cpp
@@ -1,21 +1,23 @@
 #include<iostream>
 #include "SDL.h"
+#include"Game.h"
 
 using namespace std;
 
+Game *game = nullptr;
+
 int main(int argc, char *argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window *window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	game = new Game();
+	game->init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	while (game->running()){
 
-	SDL_RenderClear(renderer);
-
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(5000);
-
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+	
+	game->clean();
 	return 0;
 }
